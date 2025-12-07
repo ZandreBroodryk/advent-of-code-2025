0566d5baea3884fe5a2cebb48c7589b74caa2487
Zandré Broodryk <z.broodryk@gmail.com>
2025-12-07 12:04:50 +0200

    Add performance upgrade for custom input

--- src/day_7.rs
+++ src/day_7.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 use crate::day_7::read_input::read_input;
 use crate::shared::{Coordinate, InputTypes, Space2D};
@@ -68,8 +68,10 @@ pub fn part_2(input: InputTypes) -> usize {
         let coordinate = Coordinate { x: x as i32, y: 0 };
         let space = input.get_coordinate(&coordinate).unwrap();
 
+        let mut cache = HashMap::new();
+
         if *space == TachyonManifoldSpace::Start {
-            let result = count_time_splits_from_position(&coordinate, &input);
+            let result = count_time_splits_from_position(&coordinate, &input, &mut cache);
             return result;
         }
     }
@@ -80,7 +82,12 @@ pub fn part_2(input: InputTypes) -> usize {
 fn count_time_splits_from_position(
     current_beam_coordinate: &Coordinate,
     area: &Space2D<TachyonManifoldSpace>,
+    cache: &mut HashMap<Coordinate, usize>,
 ) -> usize {
+    if let Some(cache_hit) = cache.get(current_beam_coordinate) {
+        return *cache_hit;
+    }
+
     let next_coordinate =
         current_beam_coordinate.get_next_in_direction(&crate::shared::Direction::Down);
     let next_space = area.get_coordinate(&next_coordinate);
@@ -88,26 +95,37 @@ fn count_time_splits_from_position(
     if let Some(space) = next_space {
         match space {
             TachyonManifoldSpace::EmptySpace => {
-                return count_time_splits_from_position(&next_coordinate, area);
+                let result = count_time_splits_from_position(&next_coordinate, area, cache);
+                cache.insert(*current_beam_coordinate, result);
+                return result;
             }
             TachyonManifoldSpace::Start => {
-                return count_time_splits_from_position(&next_coordinate, area);
+                let result = count_time_splits_from_position(&next_coordinate, area, cache);
+                cache.insert(*current_beam_coordinate, result);
+                return result;
             }
             TachyonManifoldSpace::Splitter => {
                 let option_1 =
                     next_coordinate.get_next_in_direction(&crate::shared::Direction::Left);
 
-                let time_splits_for_option_1 = count_time_splits_from_position(&option_1, area);
+                let time_splits_for_option_1 =
+                    count_time_splits_from_position(&option_1, area, cache);
 
                 let option_2 =
                     next_coordinate.get_next_in_direction(&crate::shared::Direction::Right);
 
-                let time_splits_for_option_2 = count_time_splits_from_position(&option_2, area);
+                let time_splits_for_option_2 =
+                    count_time_splits_from_position(&option_2, area, cache);
 
-                return time_splits_for_option_1 + time_splits_for_option_2;
+                let result = time_splits_for_option_1 + time_splits_for_option_2;
+                cache.insert(*current_beam_coordinate, result);
+
+                return result;
             }
             TachyonManifoldSpace::Beam => {
-                return count_time_splits_from_position(&next_coordinate, area);
+                let result = count_time_splits_from_position(&next_coordinate, area, cache);
+                cache.insert(*current_beam_coordinate, result);
+                return result;
             }
         }
     } else {
