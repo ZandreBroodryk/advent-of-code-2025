965b2b21bd6042966e54a869273b842a1e0eb66f
Zandré Broodryk <z.broodryk@gmail.com>
2025-12-11 07:00:57 +0200

    Set Up Day 12

---
+++ src/day_12/mod.rs
@@ -0,0 +1,16 @@
+use crate::day_12::read_input::read_input;
+use crate::shared::InputTypes;
+
+pub mod read_input;
+
+pub fn part_1(input: InputTypes) -> usize {
+    let input = read_input(Some(input)).expect("Error reading Input");
+
+    return input.len();
+}
+
+pub fn part_2(input: InputTypes) -> usize {
+    let input = read_input(Some(input)).expect("Error reading Input");
+
+    return input.len();
+}
